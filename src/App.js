557a30df13917fe578d5d10ccc3a4014ae1803a2
root046 <64629095+root046@users.noreply.github.com>
2023-12-06 01:02:31 +0300

    delete old components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import './App.css';
 import Counter from './components/counter/Counter';
-import AllModules from './components/learning-examples/AllComponent';
-
 
 function App() {
   return (
     <div className="App">
       My ToDo Application
-
-      <AllModules />
-      <Counter/>
+      
+      <Counter />
     </div>
   );
 }
