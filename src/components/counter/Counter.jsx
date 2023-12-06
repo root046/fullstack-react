244e0841f302c07c01f75ea5688b2bd95357e1a7
root046 <64629095+root046@users.noreply.github.com>
2023-12-06 08:19:09 +0300

    add decrement functionality

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -10,6 +10,10 @@ export default function Counter(){
         setCount(count + 1)
     }
 
+    function decrementCounterFunction(){
+        setCount(count - 1)
+    }
+
     return(
         <div className="Counter">
             <span className="count">{count}</span>
@@ -17,6 +21,10 @@ export default function Counter(){
                 <button className="counterButton" 
                     onClick={incrememntCounterFunction}
                 >+1</button>
+
+                <button className='counterButton'
+                    onClick={decrementCounterFunction}
+                >-1</button>
             </div>
         </div>
     )
