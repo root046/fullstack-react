bbcb6918ec8ff96566621d8e3d5e243b42819dec
root046 <64629095+root046@users.noreply.github.com>
2023-12-10 15:03:55 +0300

    first commit to ToDo App(this repo)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import Counter from './components/counter/Counter';
+import TodoApp from './components/todo/TodoApp'
 
 function App() {
   return (
     <div className="App">
       {/* My ToDo Application */}
+      <TodoApp/>
       
-      <Counter/>
     </div>
   );
 }
