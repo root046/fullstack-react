ec11f0ece70bb11e63c122dd2549bf49dd557b76
root046 <64629095+root046@users.noreply.github.com>
2023-12-11 15:37:18 +0300

    improving Login Component

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,24 +1,38 @@
-export default function TodoApp(){
-    return(
+import { useState } from 'react'
+import './TodoApp.css'
+export default function TodoApp() {
+    return (
         <div className="TodoApp">
             Todo Management Application
-            <LoginComponent/>
+            <LoginComponent />
             {/* <WelcomeComponent/> */}
         </div>
     )
 }
 
-function LoginComponent(){
-    return(
+function LoginComponent() {
+
+    const [username, setUsername] = useState('tset')
+    const [password, setPassword] = useState('tset')
+
+    function handleUsernameChange(event) {
+        setUsername(event.target.value)
+    }
+
+    function handlePasswordChange(event) {
+        setPassword(event.target.value)
+    }
+
+    return (
         <div className="Login">
             <div className="LoginForm">
                 <div>
                     <label>User Name</label>
-                    <input type="text" name="username"/>
+                    <input type="text" name="username" value={username} onChange={handleUsernameChange} />
                 </div>
                 <div>
                     <label>Password</label>
-                    <input type="password" name="password"/>
+                    <input type="password" name="password" value={password} onChange={handlePasswordChange} />
                 </div>
             </div>
             <div>
@@ -28,8 +42,8 @@ function LoginComponent(){
     )
 }
 
-function WelcomeComponent(){
-    return(
+function WelcomeComponent() {
+    return (
         <div className="Login">
             Welcome Component
         </div>
