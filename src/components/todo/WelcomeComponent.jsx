c10f77b4d49e24b5da1c7f168fed2c095a7e744a
root046 <64629095+root046@users.noreply.github.com>
2023-12-18 13:23:51 +0300

    Call Spring Boot Hello World REST API from React Hello World Component

--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -1,13 +1,50 @@
 import { useParams, Link } from 'react-router-dom'
+import axios from 'axios'
+import {useState} from 'react'
+
 function WelcomeComponent() {
+
     const { username } = useParams()
-    // console.log(username)
+    const [message, setMessage] = useState(null)
+
+
+    function callHelloWorldRestApi() {
+        //axios
+
+        axios.get('http://localhost:8080/hello-word', {
+            auth: {
+                username: 'bader',
+                password: '00'
+            },
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then((response) => successfulResponse(response))
+            .catch((error) => errorResponse(error))
+            .finally(() => console.log('cleanUp'))
+
+    }
+
+    function successfulResponse(response) {
+        console.log(response)
+        setMessage(response.data)
+    }
+
+    function errorResponse(error) {
+        console.log(error)
+    }
+
     return (
         <div className="Login">
             <h1>Welcome {username} To ToDo Application</h1>
             <div>
                 Manage Your Todos - <Link to='/todos'>Go Here</Link>
             </div>
+            <div>
+                <button className='btn btn-success m-5' onClick={callHelloWorldRestApi}>Call Hello World REST API</button>
+            </div>
+            <div className='text-info'>{message}</div>
         </div>
     )
 }
