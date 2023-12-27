66eb2a75f2e261f2fd1081a6297d62898ddd25c7
root046 <64629095+root046@users.noreply.github.com>
2023-12-27 22:00:08 +0300

    Integrating Spring Security JWT REST API

--- src/components/todo/api/ToDoApiService.js
+++ src/components/todo/api/ToDoApiService.js
@@ -15,12 +15,3 @@ export const updateTodoApi
 
 export const createTodoApi
     =(username,todo) => apiClient.post(`/users/${username}/todos`, todo)
-
-export const executeBasicAuthenticationService
-    =(token) => apiClient.get(`/basicauth`,
-    {
-        headers: {
-            Authorization: token
-        }
-    }
-)
--- src/components/todo/security/AuthContext.jsx
+++ src/components/todo/security/AuthContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useState } from "react";
-import {executeBasicAuthenticationService} from "../api/ToDoApiService";
+import {executeBasicAuthenticationService, executeJwtAuthenticationService} from "../api/AuthenticationApiService";
 import {apiClient} from "../api/ApiClient";
 
 //1: Create a Context
@@ -33,22 +33,56 @@ export default function AuthProvider({ children }) {
     //     }
     // }
 
+    // async function login(username, password) {
+    //
+    //     const baToken = 'Basic ' + window.btoa(username + ":" + password)
+    //
+    //     try {
+    //         const response = await executeBasicAuthenticationService(baToken);
+    //         console.log(response)
+    //         if (response.data==='Success') {
+    //             setAuthenticated(true)
+    //             setUsername(username)
+    //             setToken(baToken)
+    //
+    //             apiClient.interceptors.request.use(
+    //                 (config) => {
+    //                     console.log('intercepting and adding a token')
+    //                     config.headers.Authorization=baToken
+    //                     return config
+    //                 }
+    //             )
+    //
+    //             return true
+    //         } else {
+    //             logout()
+    //             return false
+    //         }
+    //     } catch (error) {
+    //         logout()
+    //         return false
+    //     }
+    //
+    // }
+
     async function login(username, password) {
 
-        const baToken = 'Basic ' + window.btoa(username + ":" + password)
 
         try {
-            const response = await executeBasicAuthenticationService(baToken);
+            const response = await executeJwtAuthenticationService(username,password)
             console.log(response)
-            if (response.data==='Success') {
+            if (response.status===200) {
+
+                const jwtToken = 'Bearer ' + response.data.token
+
                 setAuthenticated(true)
                 setUsername(username)
-                setToken(baToken)
+                setToken(jwtToken)
 
                 apiClient.interceptors.request.use(
                     (config) => {
                         console.log('intercepting and adding a token')
-                        config.headers.Authorization=baToken
+                        config.headers.Authorization=jwtToken
                         return config
                     }
                 )
